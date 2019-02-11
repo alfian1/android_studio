949c995fadc4224029c61c21620e09cf74396902
alfian <tokichingets@gmail.com>
2019-02-11 15:24:51 +0700

    first commit

---
+++ app/src/main/java/com/example/asusx441ua/helloword_alfian/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.asusx441ua.helloword_alfian;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
